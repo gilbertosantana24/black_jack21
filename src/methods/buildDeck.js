e2e9a2fccbd633d5db8312dd8ff5fdf7b643e8a4
Jesus Zamora <jhector.zamora@hotmail.com>
2022-12-01 22:40:39 -0800

    Added a method to build decks and the card schema definition

---
+++ src/methods/buildDeck.js
@@ -0,0 +1,26 @@
+const {cardEnum} = require("./cardSchema.js")
+const cardSchema = new cardEnum()
+
+function buildDeck(numOfDecks = 1, deck = []){
+    for(let thisValue in cardSchema.value){
+        for(let thisShape in cardSchema.shape){
+            deck.push({
+                shape:cardSchema.shape[thisShape],
+                value:cardSchema.value[thisValue]
+            });
+        }
+    }
+
+    if(numOfDecks == 1){
+        return deck
+    }
+    else{
+        return buildDeck(numOfDecks-1,deck)
+    }
+
+}
+
+var deck = buildDeck()
+console.log(deck);
+
+module.exports = {buildDeck};
---
+++ src/methods/cardSchema.js
@@ -0,0 +1,30 @@
+const Shape = {
+    Hearts:"hearts",
+    Diamonds:"diamonds",
+    Clubs:"clubs",
+    Spades:"spades"
+}
+
+const Value = {
+    1:1,
+    2:2,
+    3:3,
+    4:4,
+    5:5,
+    6:6,
+    7:7,
+    8:8,
+    9:9,
+    10:10,
+    11:11
+}
+
+let cardEnum = class  {
+    constructor(){
+    this.shape = Shape
+    this.value = Value 
+    }
+    
+}
+
+module.exports = {cardEnum};
